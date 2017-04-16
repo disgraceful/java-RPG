886403b1fcc8bca12af09ca436726b979d1a9232
disgraceful <noolic97@gmail.com>
2017-04-16 14:25:53 +0300

    Fix HealEffect Init Bug

--- src/com/disgrace/ddripoff/spells/HealEffect.java
+++ src/com/disgrace/ddripoff/spells/HealEffect.java
@@ -7,20 +7,26 @@ import javax.xml.bind.annotation.XmlRootElement;
 import com.disgrace.ddripoff.characters.shared.Character;
 import com.disgrace.ddripoff.stats.Stat;
 import com.disgrace.ddripoff.stats.StatEnumeration;
+import com.disgrace.ddripoff.stats.StatWrapper;
 import com.disgrace.ddripoff.utils.CalculationHelper;
 
 @XmlRootElement
 public class HealEffect extends Effect implements Serializable {
 
+	public HealEffect() {
+	}
+
+	public HealEffect(StatWrapper effectStats) {
+		this.effectStats = effectStats;
+	}
+
 	@Override
 	public void applyEffect(Character caller, Character target) {
-	
 		target.updateStats(calcHeal());
 	}
 
 	private Stat calcHeal() {
-		
-		double healValue = getEffectStats().getStatbyName(StatEnumeration.HEALTH).getCurValue();
+		double healValue = effectStats.getStatbyName(StatEnumeration.HEALTH).getCurValue();
 		healValue = CalculationHelper.calcRandomIntWithPercent((int) healValue, SpellConstants.CALC_DISPERSION_30);
 		return new Stat(StatEnumeration.HEALTH, (int) healValue);
 	}
--- src/com/disgrace/ddripoff/spells/TestBasicSupSpell.java
+++ src/com/disgrace/ddripoff/spells/TestBasicSupSpell.java
@@ -20,19 +20,9 @@ public class TestBasicSupSpell extends BasicSpell implements Serializable {
 		targetQuantity = SpellTargetQuantity.SINGLE;
 		abilityStats = new StatWrapper(new Stat(StatEnumeration.DAMAGE, 0), new Stat(StatEnumeration.ACC, 0),
 				new Stat(StatEnumeration.CRIT_CHANCE, 0));
-		applyingEffects.add(new HealEffect() {
-			protected StatWrapper effectStats = new StatWrapper(new Stat(StatEnumeration.HEALTH, 6));
-
-			public StatWrapper getEffectStats() {
-				return effectStats;
-			}
-
-			public void setEffectStats(StatWrapper effectStats) {
-				this.effectStats = effectStats;
-
-			}
-		});
-		System.out.println("------------------------"+applyingEffects.get(0).getEffectStats().getStatbyName(StatEnumeration.HEALTH).getCurValue());
+		applyingEffects.add(new HealEffect(new StatWrapper(new Stat(StatEnumeration.HEALTH, 6))));
+		System.out.println("------------------------"
+				+ applyingEffects.get(0).getEffectStats().getStatbyName(StatEnumeration.HEALTH).getCurValue());
 		callerRequiredPos = new SpellPositionWrapper(0, 1, 2, 3);
 		targetRequiredPos = new SpellPositionWrapper(0, 1);
 	}
