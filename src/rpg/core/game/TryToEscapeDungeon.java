44e5df0f26c12f1deaacffc5860ad0fed50f0742
disgraceful <noolic97@gmail.com>
2018-01-14 11:43:35 +0200

    hm

--- src/rpg/core/game/TryToEscapeDungeon.java
+++ src/rpg/core/game/TryToEscapeDungeon.java
@@ -1,11 +1,12 @@
 package rpg.core.game;
 
+import rpg.core.session.DungeonSession;
+
 public class TryToEscapeDungeon implements PlayerDecision{
 
 	@Override
 	public void performDecision() {
-		
-		
+		DungeonSession.fleeDungeon();
 	}
 
 	
